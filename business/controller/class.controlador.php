466de9d8b23fe19e5a5d5402a94fb63ccfa724a5
hernando.silva <hernando.silva@DESKTOP-PS7RNGS>
2017-09-18 18:18:48 -0500

    Cabeceras para cross-domain

--- business/controller/class.controlador.php
+++ business/controller/class.controlador.php
@@ -1,5 +1,8 @@
 <?php
-
+header('Access-Control-Allow-Origin: '.$_SERVER['HTTP_ORIGIN']);
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Max-Age: 1000');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
